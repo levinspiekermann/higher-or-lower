e91f6df01d9cd38edd4e42bc6a84966f91f96fa9
Fly <108137356+monolithfly@users.noreply.github.com>
2024-08-13 15:02:04 +0200

    fix: Replaced img tag with the Image component

--- app/(components)/game-item.tsx
+++ app/(components)/game-item.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function GameItem({
@@ -15,12 +16,13 @@ export default function GameItem({
     <Link href={href}>
       <div className="flex w-full flex-col items-center justify-between rounded-lg bg-white p-4 shadow-md">
         <div className="relative h-80 w-full overflow-hidden rounded-lg">
-          <img
+          <Image
             src={thumbnail}
             alt={title}
+            quality={100}
+            fill
             className="absolute inset-0 h-full w-full object-cover"
           />
-          <div className="absolute inset-0 bg-gradient-to-b from-white to-transparent opacity-50 blur-lg" />
         </div>
         <div className="mt-4 flex flex-col items-center justify-between">
           <h3 className="text-lg font-semibold text-neutral-900">{title}</h3>
