cef392c1149116dfd3f3eb2b5fd5c077c9f3f5c7
Fly <108137356+monolithfly@users.noreply.github.com>
2024-08-13 16:04:06 +0200

    style: Added z-index so the score is on top of the card

--- app/game/[game]/(components)/score-island.tsx
+++ app/game/[game]/(components)/score-island.tsx
@@ -6,7 +6,7 @@ export default function ScoreIsland({
   highscore: number;
 }) {
   return (
-    <section className="absolute top-4 flex h-[64px] items-center justify-between gap-4 p-4">
+    <section className="absolute top-4 z-10 flex h-[64px] items-center justify-between gap-4 p-4">
       <div className="flex flex-col items-center justify-center gap-0.5 border-neutral-800">
         <h1 className="text-lg font-bold text-neutral-600">Score</h1>
         <p className="text-neutral-400">{score}</p>
